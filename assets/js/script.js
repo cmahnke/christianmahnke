37c6a7ec9dcd851ec70137b0ae4b8057f99162cc
Christian Mahnke <cmahnke@gmail.com>
2021-06-06 21:02:25 +0200

    Added CSS aninmation

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,12 @@ enterView({
     offset: 0.25,
 	once: true
 });
+
+enterView({
+	selector: '.iframe-wrapper',
+	enter: function(el) {
+		el.classList.add('show');
+	},
+    offset: 0.45,
+	once: true
+});
