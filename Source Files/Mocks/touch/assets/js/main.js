86b3d6a8e8d33588fb083bf93835013b9a24df54
Christian Mahnke <cmahnke@gmail.com>
2024-09-11 20:31:11 +0200

    Updated Mock

--- Source Files/Mocks/touch/assets/js/main.js
+++ Source Files/Mocks/touch/assets/js/main.js
@@ -1,4 +1,5 @@
 const image = document.querySelector("#touch-target");
+const status = document.querySelector("#status");
 const heightMapUrl = "/page031-1.json";
 
 async function getHeights(url) {
@@ -25,23 +26,52 @@ async function generateHandler(url) {
   const initialScale = map.meta.scale;
   const clientScale = image.clientWidth / image.naturalWidth;
   const scale = initialScale * clientScale;
+  //console.log(`Calculated scales: ${clientScale} => ${scale}`);
+
   return (e) => {
-    const x = Math.floor(e.offsetX / scale);
-    const y = Math.floor(e.offsetY / scale);
+    let x, y;
+    if (e instanceof TouchEvent) {
+      const bb = e.target.getBoundingClientRect();
+      x = Math.floor((e.targetTouches[0].clientX - bb.x) / scale);
+      y = Math.floor((e.targetTouches[0].clientY - bb.y) / scale);
+    } else {
+      x = Math.floor(e.offsetX / scale);
+      y = Math.floor(e.offsetY / scale);
+    }
 
     if ("vibrate" in window.navigator) {
       if (map.data[y][x] > 0) {
         window.navigator.vibrate(200)
       }
+    } else {
+      status.innerText = "Vibrate not supported!";
     }
     //console.log(`Value at ${x} ${y}: ${map.data[y][x]}`);
   }
 
 }
 
+async function initTouch(element) {
+
+  const startTouch = (e) => {
+    e.preventDefault();
+    const current = e.changedTouches[0];
+  };
+
+  const endTouch = (e) => {
+    e.preventDefault();
+    //startTouch = [];
+
+  };
+
+  element.addEventListener("touchstart", startTouch);
+  element.addEventListener("touchend", endTouch);
+  element.addEventListener("touchcancel", endTouch);
+  element.addEventListener("touchmove", await generateHandler(heightMapUrl));
+}
 
 
 //console.log(`Loaded metadata for touch map width: ${map.width}, height: ${map.height}, scale: ${initialScale}`, map);
-//console.log(`Calculated scales: ${clientScale} => ${scale}`);
 
-image.addEventListener("mousemove", await generateHandler(heightMapUrl));
+await initTouch(image);
+//image.addEventListener("mousemove", await generateHandler(heightMapUrl));
--- Source Files/Mocks/vite.config.js
+++ Source Files/Mocks/vite.config.js
@@ -12,6 +12,7 @@ const mimeTypes = { ".glb": "model/gltf-binary" };
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {"host": "0.0.0.0"},
   base: "./",
   plugins: [
     nodePolyfills(),
