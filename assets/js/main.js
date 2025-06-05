327bb40484352831a37538c4c676bd840299f14e
Christian Mahnke <cmahnke@gmail.com>
2025-06-05 09:01:13 +0200

    Prepare menu search

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,6 @@ import { addConsent } from './iframe-consent';
 import { initMap } from './maps/osm-map.js';
 import { checkLinksForQClasses } from './wikidata';
 
-/* See https://github.com/rgalus/sticky-js */
-/* window.Sticky = require('sticky-js'); */
 import stickybits from 'stickybits'
 window.stickybits = stickybits;
 
@@ -23,11 +21,32 @@ window.enterView = require('enter-view');
 
 import Elevator from 'elevator.js';
 
-window.onload = function() {
+window.addEventListener('DOMContentLoaded', (event) => {
   var elevator = new Elevator({
     mainAudio: '/sounds/elevator.mp3',
     endAudio: '/sounds/ding.mp3',
     element: document.querySelector('.backToTop'),
     duration: 2000
   });
-}
+
+  const searchButton = document.querySelector("#menu-search-button");
+  const searchInput = document.querySelector("#menu-search-input");
+  const searchForm = document.querySelector("#menu-search-form");
+  if (searchForm !== null) {
+    let focused = false;
+    searchInput.addEventListener("focus", () => {
+      focused = true;
+    })
+    searchButton.addEventListener("click", (event) => {
+      console.log("got click")
+      if (searchInput.value != "") {
+        searchForm.submit();
+
+      } else if (searchInput.value == "" && !focused) {
+        searchInput.focus();
+        event.preventDefault();
+        event.stopImmediatePropagation();
+      }
+    })
+  }
+});
