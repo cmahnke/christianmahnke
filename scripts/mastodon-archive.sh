cae27b0bc52e62c194e353101460ab8049ebe603
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-01-16 17:56:23 +0100

    Updated theme

--- scripts/mastodon-archive.sh
+++ scripts/mastodon-archive.sh
@@ -7,10 +7,16 @@ fi
 if [ -z "$BACKUP_DIRECTORY" ] ; then
   BACKUP_DIRECTORY="./data/mastodon"
 fi
+rm -rf $BACKUP_DIRECTORY/*
 
 if [ -z "$BACKUP_FILE" ] ; then
   BACKUP_FILE="openbiblio.social.user.cmahnke.json"
 fi
+
+IMAGE_DIRECTORY=$(basename $BACKUP_FILE .json)
+ASSET_DIRECTORY=./assets/mastodon/media
+rm -rf "$ASSET_DIRECTORY"
+#mkdir -p "$ASSET_DIRECTORY"
 
 git config --global remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git ls-remote --exit-code origin $BACKUP_BRANCH
@@ -18,8 +24,9 @@ EXIT_CODE=$?
 if [ $EXIT_CODE -eq 0 ] ; then
   git fetch origin $BACKUP_BRANCH
   git checkout origin/$BACKUP_BRANCH -- "$BACKUP_DIRECTORY/*.json"
-  git restore --staged "$BACKUP_DIRECTORY/*.json"
-  #git show $BACKUP_BRANCH:$BACKUP_DIRECTORY/$BACKUP_FILE > $BACKUP_DIRECTORY/$BACKUP_FILE
+  git checkout origin/$BACKUP_BRANCH -- "$BACKUP_DIRECTORY/$IMAGE_DIRECTORY"
+  git restore --staged "$BACKUP_DIRECTORY"
+  mv "$BACKUP_DIRECTORY/$IMAGE_DIRECTORY" "$ASSET_DIRECTORY"
 else
   echo "No backup branch '$BACKUP_BRANCH' found, skipping checkout."
 fi
