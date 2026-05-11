09ada5523e625144449fb89562b7449e8f259145
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-05-11 17:36:15 +0200

    Updated dependency

--- Source Files/Experiments/article-pdf/generate_article_pdfs.sh
+++ Source Files/Experiments/article-pdf/generate_article_pdfs.sh
@@ -13,7 +13,7 @@ find docs/post/ -name article.html | while read -r FILE_PATH; do
     # FILE_PATH: The initial file path (e.g., docs/post/my-post/article.html)
     # BASE_PATH_NAME: The path and base name without the extension (e.g., docs/post/my-post/article)
     BASE_PATH_NAME="${FILE_PATH%.*}"
-    echo "Prpcessing $FILE_PATH to $BASE_PATH_NAME.pdf"
+    echo "Processing $FILE_PATH to $BASE_PATH_NAME.pdf"
     NODE_PATH="$VIVLIOSTYLE_NODE_PATH" node "$VIVLIOSTYLE" -i "$FILE_PATH" --asset-base https://christianmahnke.de/=./docs/ --asset-base http://localhost:1313/=./docs/ --ignore-asset /livereload.js -o "${BASE_PATH_NAME}.pdf"
 done
 
--- scripts/article-pdfs.sh
+++ scripts/article-pdfs.sh
@@ -8,14 +8,14 @@ if [ ! -d "$SEARCH_DIR" ]; then
     exit 1
 fi
 
-echo "searcing for 'article.html' in '$SEARCH_DIR'..."
+echo "Searching for 'article.html' in '$SEARCH_DIR'..."
 
 find "$SEARCH_DIR" -type f -name "article.html" | while read -r html_file; do
     pdf_file="${html_file%.html}-print.pdf"
     pdf_file_web="${html_file%.html}.pdf"
 
     echo "Converting '$html_file' zu '$pdf_file'..."
-    node viviostyle-cli -i "$html_file" --asset-base https://christianmahnke.de/=./docs/ --asset-base http://localhost:1313/=./docs/ --ignore-asset /livereload.js -o "$pdf_file"
+    npx viviostyle-batch-cli -i "$html_file" --asset-base https://christianmahnke.de/=./docs/ --asset-base http://localhost:1313/=./docs/ --ignore-asset /livereload.js -o "$pdf_file"
     if [ $? -eq 0 ]; then
         echo "'$pdf_file' created successfully"
     else
