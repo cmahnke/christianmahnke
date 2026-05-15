6bcda8ef0f7c98a1e6aeaba6dc552d65818e626b
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-05-15 08:21:41 +0200

    Fix figures on PDF articles

--- scripts/article-pdfs.sh
+++ scripts/article-pdfs.sh
@@ -3,6 +3,8 @@
 SEARCH_DIR="docs"
 SCRIPT_LOCATION=`dirname "$(realpath $0)"`
 
+$ARGS="$@"
+
 hugo --renderSegments print
 
 if [ ! -d "$SEARCH_DIR" ]; then
@@ -12,12 +14,14 @@ fi
 
 echo "Searching for 'article.html' in '$SEARCH_DIR'..."
 
-find "$SEARCH_DIR" -type f -name "article.html" | while read -r html_file; do
+
+find $SEARCH_DIR -type f -name "article.html" | while read -r html_file; do
     pdf_file="${html_file%.html}-print.pdf"
     pdf_file_web="${html_file%.html}.pdf"
 
     echo "Converting '$html_file' zu '$pdf_file'..."
-    npx vivliostyle-batch-cli -i "$html_file" --asset-base https://christianmahnke.de/=./docs/ --asset-base http://localhost:1313/=./docs/ --ignore-asset /livereload.js -o "$pdf_file"
+    #npx vivliostyle-batch-cli -i "$html_file" --asset-base https://christianmahnke.de/=./docs/ --asset-base http://localhost:1313/=./docs/ --ignore-asset /livereload.js -o "$pdf_file"
+    npx vivliostyle-batch-cli -i "$html_file" --static /:./docs/ --asset-base https://christianmahnke.de/=./docs/ --asset-base http://localhost:1313/=./docs/ --ignore-asset /livereload.js -o "$pdf_file" $@
     if [ $? -eq 0 ]; then
         echo "'$pdf_file' created successfully"
     else
