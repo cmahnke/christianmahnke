30abd3defd8288c5090de0fa934b4832fbf56b73
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-08-22 12:26:40 +0200

    Try to fix git deployment

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -2,10 +2,11 @@
 
 CTX_PATH="$(dirname $(realpath $0))"
 
-$CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.11.1.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.12.0.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-inkscape.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-media.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/git-setup.sh
