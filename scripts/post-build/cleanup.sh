b405db87ee3064120cab1638ba0fe1d9a943bee9
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-08-24 18:49:24 +0200

    Updated cleanup

--- scripts/post-build/cleanup.sh
+++ scripts/post-build/cleanup.sh
@@ -30,4 +30,8 @@ done
 # TODO enable this - make sure it won't kill hdr stuff
 #find $SEARCH_PATH -type f -path '*/full/full/0/default.jpg' -not -path '*-hdr*' -exec mogrify -quality 90 {} \;
 
+echo "Removing JXL images from output"
 find $SEARCH_PATH -type f -path '*.jxl' -not -path '*hdr*' -exec rm {} \;
+echo "Removing post specific output"
+set -x
+rm docs/post/hang-pictures-in-sweethome3d/img/Gallerie-Alte-Post-plan.svg
